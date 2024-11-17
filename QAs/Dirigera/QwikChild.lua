9adc802e52f42d1a0445e9daf1fdf1e5a7fc3b21
Jan Gabrielsson <jan@gabrielsson.com>
2024-11-17 09:15:12 +0100

    Removed unnecessary debug print statement

--- QAs/Dirigera/QwikChild.lua
+++ QAs/Dirigera/QwikChild.lua
@@ -1,12 +1,12 @@
------------ Child class
 do
   local childID = 'ChildID'
   local classID = 'ClassName'
-  local defChildren
 
   local children = {}
-  local undefinedChildren = {}
   local createChild = QuickApp.createChildDevice
+  function QuickApp:initChildDevices() end
+  QuickApp.debugQwikAppChild = true
+
   class 'QwikAppChild'(QuickAppChild)
 
   local fmt = string.format
@@ -16,34 +16,13 @@ do
     if stat ~= 200 then return nil end
     return res.value
   end
-  
-  local function setupUIhandler(self)
-    if not self.UIHandler then
-      function self:UIHandler(event)
-        local obj = self
-        if self.id ~= event.deviceId then obj = (self.childDevices or {})[event.deviceId] end
-        if not obj then return end
-        local elm,etyp = event.elementName, event.eventType
-        local cb = obj.uiCallbacks or {}
-        if obj[elm] then return obj:callAction(elm, event) end
-        if cb[elm] and cb[elm][etyp] and obj[cb[elm][etyp]] then return obj:callAction(cb[elm][etyp], event) end
-        if obj[elm.."Clicked"] then return obj:callAction(elm.."Clicked", event) end
-        self:warning("UI callback for element:", elm, " not found-")
-      end
-    end
-  end
 
   local UID = nil
   function QwikAppChild:__init(device)
     QuickAppChild.__init(self, device)
-    self:debug(fmt("Instantiating ID:%s '%s'",device.id,device.name))
     local uid = UID or self:internalStorageGet(childID) or ""
     self._uid = uid
-    --if defChildren[uid] then
-      children[uid]=self               -- Keep table with all children indexed by uid. uid is unique.
-    --else                               -- If uid not in our children table, we will remove this child
-    --  undefinedChildren[#undefinedChildren+1]=self.id
-    --end
+    children[uid]=self
     self._sid = tonumber(uid:match("(%d+)$"))
   end
 
@@ -52,8 +31,8 @@ do
     __assert_type(className,'string')
     props.initialProperties = props.initialProperties or {}
     props.initialInterfaces = interfaces
-    --self:debug("Creating device ",props.name)
     UID = uid
+    print(json.encode(props))
     local c = createChild(self,props,_G[className])
     UID = nil
     if not c then return end
@@ -66,23 +45,25 @@ do
     __assert_type(chs,'table')
     local rerr = false
     local stat,err = pcall(function()
-      defChildren = chs
       self.children = children
-      function self.initChildDevices() end
       local cdevs,n = api.get("/devices?parentId="..self.id) or {},0 -- Pick up all my children
       for _,child in ipairs(cdevs) do
         local uid = getVar(child.id,childID)
         local className = getVar(child.id,classID)
-        print(child.id,uid,className)
         local childObject = nil
-        local stat,err = pcall(function()
-          childObject = _G[className] and _G[className](child) or QuickAppChild(child)
-          self.childDevices[child.id]=childObject
-          childObject.parent = self
-        end)
-        if not stat then
-          self:error("loadExistingChildren:"..err)
-          rerr=true
+        if chs[uid] then
+          if QuickApp.debugQwikAppChild then
+            self:debug(fmt("Loading existing child UID:'%s'",uid))
+          end
+          local stat,err = pcall(function()
+            childObject = _G[className] and _G[className](child) or QuickAppChild(child)
+            self.childDevices[child.id] = childObject
+            childObject.parent = self
+          end)
+          if not stat then
+            self:error(fmt("loadExistingChildren:%s child UID:%s",err,uid))
+            rerr=true
+          end
         end
       end
     end)
@@ -90,17 +71,20 @@ do
     return rerr
   end
 
-  function QuickApp:createMissingChildren()
+  function QuickApp:createMissingChildren(children)
     local stat,err = pcall(function()
       local chs,k = {},0
-      for uid,data in pairs(defChildren) do
+      for uid,data in pairs(children) do
         local m = uid:sub(1,1)=='i' and 100 or 0
         k = k + 1
         chs[#chs+1]={uid=uid,id=m+tonumber(uid:match("(%d+)$") or k),data=data}
       end
       table.sort(chs,function(a,b) return a.id<b.id end)
       for _,ch in ipairs(chs) do
-        if not self.children[ch.uid] then
+        if not self.children[ch.uid] then -- not loaded yet
+          if QuickApp.debugQwikAppChild then
+            self:debug(fmt("Creating missing child UID:'%s'",ch.uid))
+          end
           local props = {
             name = ch.data.name,
             type = ch.data.type,
@@ -113,16 +97,21 @@ do
     if not stat then self:error("createMissingChildren:"..err) end
   end
 
-  function QuickApp:removeUndefinedChildren()
-    for _,deviceId in ipairs(undefinedChildren) do -- Remove children not in children table
-      self:removeChildDevice(deviceId)
+  function QuickApp:removeUndefinedChildren(children)
+    local cdevs = api.get("/devices?parentId="..self.id)
+    for _,child in ipairs(cdevs) do
+      if not self.childDevices[child.id] then
+        if QuickApp.debugQwikAppChild then
+          self:debug(fmt("Deleting undefined child ID:%s",child.id))
+        end
+        api.delete("/plugins/removeChildDevice/" .. child.id)
+      end
     end
   end
 
   function QuickApp:initChildren(children)
-    setupUIhandler(self)
     if self:loadExistingChildren(children) then return end
-    self:createMissingChildren()
-    self:removeUndefinedChildren()
+    self:createMissingChildren(children)
+    self:removeUndefinedChildren(children) -- Remove child devices not loaded/created
   end
 end
